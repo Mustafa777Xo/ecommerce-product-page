0362c6cb9fadcde3906159f5962634a6948bc212
Mustafa777Xo <mustafa777bdo@gmail.com>
2023-08-27 15:30:02 +0200

    fixing cart compenet on mobile device

--- src/components/NavBar/Cart.vue
+++ src/components/NavBar/Cart.vue
@@ -1,8 +1,8 @@
 <template>
   <img @click="showingCart" :src="CartSVG" class="cart-img" />
   <v-card v-if="showCart" title="Cart" class="cart-card">
-      <v-divider :thickness="2" class="my-2"></v-divider>
-      <p class="main-body">Your cart is empty</p>
+    <v-divider :thickness="2" class="my-2"></v-divider>
+    <p class="main-body">Your cart is empty</p>
   </v-card>
 </template>
 <script setup lang="ts">
@@ -27,11 +27,27 @@ function showingCart() {
   width: 400px;
   height: 300px;
   background-color: $White;
-  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px,
+    rgba(0, 0, 0, 0.22) 0px 10px 10px;
+  @media only screen and (max-width: 768px) {
+    left: 55%;
+    top: 8%;
+    width: 300px;
+    height: 200px;
+  }
+  @media only screen and (max-width: 390px) {
+    left: 8%;
+    top: 10%;
+    width: 330px;
+    height: 200px;
+  }
 }
 .main-body {
   color: $Dark-grayish-blue;
   font-weight: bold;
   margin: 25% 30%;
+  @media only screen and (max-width: 768px) {
+    margin: 15% 23%;
+  }
 }
 </style>
