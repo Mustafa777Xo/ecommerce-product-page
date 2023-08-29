2d038633ec68c4006c33eb876e3d4a13a3067cbb
Mustafa777Xo <mustafa777bdo@gmail.com>
2023-08-29 18:12:14 +0200

    fixed overlay

--- src/components/Gallery/Gallery.vue
+++ src/components/Gallery/Gallery.vue
@@ -1,6 +1,11 @@
 <template>
   <v-col>
-    <img :src="productImages[selectedId].img" alt="" class="product-img" />
+    <img
+      :src="productImages[selectedId].img"
+      alt=""
+      class="product-img"
+      @click="showingOverlay"
+    />
     <div class="d-flex">
       <div v-for="productImg in productImages" class="thumb-container">
         <img
@@ -15,13 +20,30 @@
       </div>
     </div>
   </v-col>
+  <div id="overlay" v-if="showOverlay">
+    <v-container class="overlay-container">
+      <v-carousel hide-delimiters>
+        <v-carousel-item
+          v-for="productImage in productImages"
+          :src="productImage.img"
+          class="cur"
+        ></v-carousel-item>
+      </v-carousel>
+    </v-container>
+  </div>
 </template>
 <script lang="ts" setup>
 import { productImages } from "@/TypesAndData/Data";
 import { ref } from "vue";
 
 let selectedId = ref<number>(0);
+let showOverlay = ref<boolean>(false);
+
 let isSelectedThumb = ref<boolean>(false);
+
+const showingOverlay = () => {
+  showOverlay.value = true;
+};
 const selectedImgId = (id: number) => {
   selectedId.value = id;
   isSelectedThumb.value = true;
@@ -42,5 +64,24 @@ const selectedImgId = (id: number) => {
 .selected-thumb {
   border: 3px $Orange solid;
   opacity: 0.8;
+}
+#overlay {
+  position: fixed; /* Sit on top of the page content */
+  display: block; /* Hidden by default */
+  width: 100%; /* Full width (cover the whole page) */
+  height: 100%; /* Full height (cover the whole page) */
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5); /* Black background with opacity */
+  z-index: 2; /* Specify a stack order in case you're using a different order for other elements */
+  cursor: pointer; /* Add a pointer on hover */
+}
+.overlay-container {
+  position: absolute;
+  top: 15%;
+  left: 40%;
+  width: 33%;
 }
 </style>
