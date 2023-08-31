928db4d301b948feed141ffa125e36e7465ad05a
Mustafa777Xo <mustafa777bdo@gmail.com>
2023-08-31 17:40:46 +0200

    fixed gallery compnent

--- src/components/Gallery/Gallery.vue
+++ src/components/Gallery/Gallery.vue
@@ -20,45 +20,73 @@
       </div>
     </div>
   </v-col>
-  <div id="overlay" v-if="showOverlay">
-    <v-container class="overlay-container">
-      <v-carousel hide-delimiters>
-        <v-carousel-item
-          v-for="productImage in productImages"
-          :src="productImages[selectedId].img"
-          class="cur"
-        ></v-carousel-item>
-      </v-carousel>
-    </v-container>
-    <div class="d-flex overlay-thumb">
-      <div v-for="productImg in productImages" class="thumb-container">
-        <img
-          :src="productImg.thumbnail"
-          alt=""
-          :class="{
-            'selected-thumb': isSelectedThumb && selectedId === productImg.id,
-          }"
-          class="product-thumbnail my-5 mr-4"
-          @click="selectedImgId(productImg.id)"
-        />
-      </div>
-    </div>
+  <div
+    class="overlay"
+    :class="{ 'hide-overlay': hideOverlay }"
+    v-if="showOverlay"
+  >
+    <v-col>
+      <img class="close-icon" @click="hidingOverlay" :src="CloseIcon" alt="" />
+      <v-container class="overlay-container">
+        <Carousel
+          id="gallery"
+          :items-to-show="1"
+          :wrap-around="false"
+          v-model="selectedId"
+        >
+          <Slide v-for="productImage in productImages" :key="productImage.id">
+            <img
+              class="carousel__item_gallery"
+              :src="productImage.img"
+              alt=""
+            />
+          </Slide>
+        </Carousel>
+
+        <Carousel
+          id="thumbnails"
+          :items-to-show="4"
+          :wrap-around="true"
+          v-model="selectedId"
+          ref="carousel"
+        >
+          <Slide v-for="productImage in productImages" :key="productImage.id">
+            <img
+              class="carousel__item"
+              @click="selectedImgId(productImage.id - 1)"
+              :src="productImage.thumbnail"
+              alt=""
+            />
+          </Slide>
+        </Carousel>
+      </v-container>
+    </v-col>
   </div>
 </template>
 <script lang="ts" setup>
+import { Carousel, Slide } from "vue3-carousel";
+
+import "vue3-carousel/dist/carousel.css";
 import { productImages } from "@/TypesAndData/Data";
 import { ref } from "vue";
+import CloseIcon from "@/assets/icon-close.svg";
 
 let selectedId = ref<number>(0);
 let showOverlay = ref<boolean>(false);
+let hideOverlay = ref<boolean>(false);
 
 let isSelectedThumb = ref<boolean>(false);
 
 const showingOverlay = () => {
   showOverlay.value = true;
+  hideOverlay.value = false;
+};
+const hidingOverlay = () => {
+  hideOverlay.value = true;
 };
 const selectedImgId = (id: number) => {
   selectedId.value = id;
+
   isSelectedThumb.value = true;
 };
 </script>
@@ -78,7 +106,7 @@ const selectedImgId = (id: number) => {
   border: 3px $Orange solid;
   opacity: 0.8;
 }
-#overlay {
+.overlay {
   position: fixed; /* Sit on top of the page content */
   display: block; /* Hidden by default */
   width: 100%; /* Full width (cover the whole page) */
@@ -91,11 +119,25 @@ const selectedImgId = (id: number) => {
   z-index: 2; /* Specify a stack order in case you're using a different order for other elements */
   cursor: pointer; /* Add a pointer on hover */
 }
+.hide-overlay {
+  display: none; /* Hidden by default */
+}
+.close-icon {
+margin-left: 73%;
+  width: 1rem;
+}
 .overlay-container {
   position: absolute;
-  top: 15%;
-  left: 35%;
-  width: 33%;
+  padding: 0;
+  top: 5%;
+  width: 52%;
+  left: 25%;
+}
+.carousel__item_gallery {
+  height: 40rem;
+}
+.carousel__item {
+  margin-top: 1rem;
 }
 .overlay-thumb {
   position: absolute;
