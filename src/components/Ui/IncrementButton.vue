8c200f3da8d8ff6c59f4df9f140f1954722b0b82
Mustafa777Xo <mustafa777bdo@gmail.com>
2023-09-12 07:56:24 +0200

    increament btn fixed

--- src/components/Ui/IncrementButton.vue
+++ src/components/Ui/IncrementButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-flex container">
+  <div class="container">
     <div class="minus-icon">
       <img :src="MinusIcon" />
     </div>
@@ -23,19 +23,21 @@ let conuter = ref<number>(0);
 </script>
 <style lang="scss" scoped>
 .container {
-  padding: 1rem 0.5rem;
   background-color: $Light-grayish-blue;
   border-radius: 0.5rem;
-  @media only screen and (min-width: 768px) {
-    width: 10rem;
+  display: flex;
+  flex-direction: row;
+  gap: 3rem;
+  padding: 1.2rem 0.7rem;
+  width: 50%;
+  height: 80%;
+  @media only screen and (max-width: 768px) {
+    gap: 8rem;
+    padding: 1.2rem 1rem;
   }
-
-}
-.minus-icon{
-margin-right: 50%;
-}
-.counter{
-margin-right: 40%;
-
+  @media only screen and (max-width: 390px) {
+    gap: 8rem;
+    width: 100%;
+  }
 }
 </style>
