04f96f1b86731433c509fd330a68b96218c34609
Mustafa777Xo <mustafa777bdo@gmail.com>
2023-09-15 17:47:10 +0200

    loader init

---
+++ src/components/Ui/Loader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="spinner"></div>
+</template>
+<style scoped lang="scss">
+spinner {
+  position: relative;
+  width: 22.4px;
+  height: 22.4px;
+}
+
+.spinner::before,
+.spinner::after {
+  content: "";
+  width: 100%;
+  height: 100%;
+  display: block;
+  animation: spinner-b4c8mmmd 0.5s backwards,
+    spinner-49opz7md 1.25s 0.5s infinite ease;
+  border: 5.6px solid #ff7d1a;
+  border-radius: 50%;
+  box-shadow: 0 -33.6px 0 -5.6px #ff7d1a;
+  position: absolute;
+}
+
+.spinner::after {
+  animation-delay: 0s, 1.25s;
+}
+
+@keyframes spinner-b4c8mmmd {
+  from {
+    box-shadow: 0 0 0 -5.6px #ff7d1a;
+  }
+}
+
+@keyframes spinner-49opz7md {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
